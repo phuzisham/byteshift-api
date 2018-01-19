de1d227f29025cea3c5977407803f0b0e5074dbc
Dawson M <dawson.mortenson@gmail.com>
2018-01-19 12:07:23 -0800

    setup inital index for items based on existence

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -2,5 +2,7 @@ class API::ItemsController < ApplicationController
   include Response
 
   def index
+    @items = params[:category_id] ? Item.where(category_id: params[:category_id]) : Item.where(store_id: params[:store_id])
+    json_response(@items)
   end
 end
