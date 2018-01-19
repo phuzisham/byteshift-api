eb8e6bca14ddaeede66c72203dfa5c645aa77cec
Dawson M <dawson.mortenson@gmail.com>
2018-01-19 12:38:14 -0800

    change catergory to category_id in the items index

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -3,7 +3,7 @@ class API::ItemsController < ApplicationController
 
   def index
     # Finds a category if the params exist
-    category = Category.find(params[:category]) if params[:category]
+    category = Category.find(params[:category]) if params[:category_id]
     # If the category is found then it grabs the items from that category otherwise is defaults to using the store to find its items!
     @products = category ? category.items : Store.find(params[:store_id]).items
     json_response(@products)
