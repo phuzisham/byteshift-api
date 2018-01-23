04feec067bc080fec19c401be128e087ab380c66
Dawson M <dawson.mortenson@gmail.com>
2018-01-23 10:46:28 -0800

    begin to define the user model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,13 @@
 class Account < ApplicationRecord
-  # belongs_to :user
+  belongs_to :user
   has_many :lists
+  before_save :format_name
+
+  validates_presence_of :name, :user_id
+
+  private
+
+  def format_name
+    self.name = name.downcase.titleize
+  end
 end
