707db266e32f8a7786e54e5b85ae8ab5eda95e4f
Dawson M <dawson.mortenson@gmail.com>
2018-01-24 13:12:33 -0800

    setup list_items index for a lists items and add a todo to the routes config

--- app/controllers/api/list_items_controller.rb
+++ app/controllers/api/list_items_controller.rb
@@ -1,2 +1,9 @@
 class API::ListItemsController < ApplicationController
+  include Response
+
+  def index
+    list = List.find(params[:list_id])
+    @list_items = list.list_items
+    json_response(@list_items)
+  end
 end
