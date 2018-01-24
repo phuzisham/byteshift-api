1478a4a4a98fb0ed78925c92aa8f0efc4f3bbc2f
Dawson M <dawson.mortenson@gmail.com>
2018-01-24 12:32:17 -0800

    complete the lists show by finding a given list

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,4 +1,5 @@
 class API::ListsController < ApplicationController
+  # Implement auth on these
   include Response
 
   # Retrieve an individual users lists
@@ -6,5 +7,10 @@ class API::ListsController < ApplicationController
     user = Account.find(params[:account_id])
     @lists = user.lists
     json_response(@lists)
+  end
+
+  def show
+    @list = List.find(params[:id])
+    json_response(@list)
   end
 end
