2c894050a56cb91bfe99b7ed7a54af18d04a5cac
Dawson M <dawson.mortenson@gmail.com>
2018-01-24 13:16:32 -0800

    add list_items create, define a private params method, and comment out show with commented explanation

--- app/controllers/api/list_items_controller.rb
+++ app/controllers/api/list_items_controller.rb
@@ -6,4 +6,22 @@ class API::ListItemsController < ApplicationController
     @list_items = list.list_items
     json_response(@list_items)
   end
+
+  # Not sure we need a show?
+  # It could be a link to the actual item to eliminate redundancy
+  # def show
+  #
+  # end
+
+  def create
+    list = List.find(params[:list_id])
+    @list_item = list.list_items.new(list_items_params)
+    @list_item.save ? json_response(@list_item) : json_response(@list_item.errors, status = :not_acceptable)
+  end
+
+  private
+
+  def list_items_params
+    params.permit(:list_id, :item_id, :name)
+  end
 end
