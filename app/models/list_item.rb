cba26187a11c98ee0dbd85cde366024a2fd5960a
Dawson M <dawson.mortenson@gmail.com>
2018-01-26 12:51:06 -0800

    patch to list model and complete list_item validations

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -2,5 +2,21 @@ class ListItem < ApplicationRecord
   belongs_to :list
   has_one :item
 
-  validates_presence_of :name, :list_id, :item_id
+  validates_presence_of :name
+
+  validates :list_id, presence: true, numericality: { only_integer: true }
+  validates :item_id, presence: true, numericality: { only_integer: true }
+
+  validate_list_id :list_id
+  validate_item_id :item_id
+
+  private
+
+  def validate_list_id
+    errors.add(:list_id, "doesn't exist") unless List.exists?(list_id)
+  end
+
+  def validate_item_id
+    errors.add(:item_id, "doesn't exist") unless Item.exists?(item_id)
+  end
 end
