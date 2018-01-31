cca123ef35a9edc9c3719b9b243e2497639abe89
Dawson M <dawson.mortenson@gmail.com>
2018-01-30 23:19:48 -0800

    add validation specs to the list model

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -5,3 +5,10 @@ describe List, 'relations' do
   it { should belong_to :store }
   it { should have_many :list_items }
 end
+
+describe List, 'validations' do
+  it { should validate_presence_of :account_id }
+  it { should validate_presence_of :store_id }
+  it { should validate_numericality_of(:account_id).only_integer }
+  it { should validate_numericality_of(:store_id).only_integer }
+end
