fdac3ee8ccd242b8b7b39f879dc794143e48cc48
Dawson M <dawson.mortenson@gmail.com>
2018-01-30 23:16:35 -0800

    add relation specs for the list model

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe List, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe List, 'relations' do
+  it { should belong_to :account }
+  it { should belong_to :store }
+  it { should have_many :list_items }
 end
